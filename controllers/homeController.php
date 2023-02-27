d9bb1e38f02d36ef36e19880023d3f2438c432fb
EvilCorpRobot <theo.conte38250@gmail.com>
2023-02-27 16:34:04 +0100

    adding header.html and footer.html

---
+++ controllers/homeController.php
@@ -0,0 +1,22 @@
+<?php
+
+function calculCalorie($weight, $size, $age, $sex, $sportiveActivity) {
+    $resultWeight = $weight * 10;
+    $resultSize = $size * 6.25;
+    $result = $resultWeight + $resultSize;
+    $result = $result - ($age * 5);
+    if ($sex == 0) {
+        $result -= 161;
+    } else {
+        $result += 5;
+    }
+
+    if ($sportiveActivity == 0) {
+        $result *= 1.2;
+    } elseif ($sportiveActivity == 1) {
+        $result *= 1.55;
+    } else {
+        $result *= 1.725;
+    }
+    return $result;
+}
