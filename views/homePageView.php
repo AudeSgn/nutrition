a55d60515f9a7b1687d76dd26a291c6a5673e138
Colas Rabiller <colas.rabiller@gmail.com>
2023-03-01 13:02:30 +0100

    template file

---
+++ views/homePageView.php
@@ -0,0 +1,134 @@
+<?php
+$title = 'Homepage';
+
+ob_start(); ?>
+<!-- <div class="container"> -->
+<div id="buttonsRound">
+  <div id="btnImc">
+    <div class="insideButtonsRound">
+      <p>IMC :</p>
+      <p id="imcNumber">30</p>
+      <p id="imcType">Normal</p>
+      <button type="button" name="btnDetails" id="btnDetails" data-bs-toggle="modal" data-bs-target="#modalDetailsImc">Détails</button>
+    </div>
+  </div>
+  <div id="btnCalories">
+    <div class="insideButtonsRound">
+      <p>Calories/j</p>
+      <p id="numberCalories">360</p>
+      <p>kcal</p>
+    </div>
+  </div>
+</div>
+
+<div id="desktopVersion">
+
+  <div id="buttonsMeals">
+    <div id="breakfast" class="allBtnMeal">
+      <button type="button" name="btnBreakfast" id="btnBreakfast" class="btnMeal" data-bs-toggle="modal" data-bs-target="#modalMeals">
+        <img src="../assets/img/breakfast.png" class="imgMeal" />
+      </button>
+      <p class="typeMeal">Petit-déjeuner</p>
+    </div>
+    <div id="diner" class="allBtnMeal">
+      <button type="button" name="btnDiner" id="btnDiner" class="btnMeal" data-bs-toggle="modal" data-bs-target="#modalMeals">
+        <img src="../assets/img/diner.png" class="imgMeal" />
+      </button>
+      <p class="typeMeal">Diner</p>
+    </div>
+    <div id="lunch" class="allBtnMeal">
+      <button type="button" name="btnLunch" id="btnLunch" class="btnMeal" data-bs-toggle="modal" data-bs-target="#modalMeals">
+        <img src="../assets/img/salad.png" class="imgMeal" />
+      </button>
+      <p class="typeMeal">Déjeuner</p>
+    </div>
+    <div id="snack" class="allBtnMeal">
+      <button type="button" name="btnSnack" id="btnSnack" class="btnMeal" data-bs-toggle="modal" data-bs-target="#modalMeals">
+        <img src="../assets/img/tea.png" class="imgMeal" />
+      </button>
+      <p class="typeMeal">Encas</p>
+    </div>
+  </div>
+
+  <div id="graphCalories">
+    <canvas id="barCanvas" aria-label="chart"></canvas>
+
+    <div id="historic">
+      <a href="">
+        <button type="button" name="btnHistoric" class="button" id="btnHistoric">Voir mes repas</button>
+      </a>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- ..............................Modal IMC-->
+
+<div class="modal fade" id="modalDetailsImc" tabindex="-1" aria-labelledby="modalDetailsImc" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header border border-0">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-bodyImc">
+        <h1 class="modal-title fs-5 mb-" id="titleImcModal">Ton IMC</h1>
+        <img src="../assets/img/imcDiagram.jpg" id="imgImcModal" />
+        <p id="descriptionImcModal">
+          L'IMC est le sigle pour Indice de Masse Corporelle. L'IMC permet de calculer la corpulence d'un individu selon sa taille et son poids. L'IMC est utilisé pour détecter les problèmes
+          d'obésité ou de sous-poids.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- .................................Modal buttons Meals -->
+<div class="modal fade" id="modalMeals" tabindex="-1" aria-labelledby="modalMeals" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header border border-0" id="modalMealHeader">
+        <div class="titleMeal"></div>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modalMeal-body">
+        <div id="inputDate">
+          <input type="date" id="dateMealModal" value="<?php echo date('Y-m-d'); ?>" />
+        </div>
+        <form method="post" action="" id="formMeal">
+          <div id="numberOfCalories">
+            <label for="numberCaloriesForm" class="form-label">Nombre de calories</label>
+            <input type="text" class="form-control mb-4" id="numberCaloriesForm" name="calories" placeholder="Indique le nombre de calories de ton repas" />
+          </div>
+          <div id="mealDetails">
+            <label for="mealDescriptionForm" class="form-label">Nombre de calories</label>
+            <textarea type="text" class="form-control" id="mealDescriptionForm" name="description" placeholder="Décris ton repas"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer border border-0">
+        <button type="button" class="btnModalMealForm" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#modalAlertCalories">Valider</button>
+        <button type="button" class="btnModalMealForm">Annuler</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- .........................................Modal alerte nombre calories dépassées -->
+<div class="modal fade" id="modalAlertCalories" tabindex="-1" aria-labelledby="modalAlertCalories" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content" id="modalAlertContent">
+      <div class="modal-header border border-0">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <h1 class="modal-title fs-5 mb-" id="titleImcModal">ATTENTION</h1>
+        <p id="alertCaloriesModal">Tu as dépassé ton apport en calorie journalier recommandé !</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- </div> -->
+<?php $content = ob_get_clean();
+require 'template.php';
+?>
