1362af76113e37635e36a5aa28ba9678f5d3e551
Colas Rabiller <colas.rabiller@gmail.com>
2023-03-06 10:02:44 +0100

    calcul imc

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,5 +1,11 @@
 <?php
-
+require_once './models/userModel.php';
+$weight = $user['user_id'];
+$size = $user['size'];
+$today = date("y.m.d");
+$age = date_diff(date_create($user['birthdate']), date_create($today));
+$sex = $user['sex'];
+$sportiveActivity = $user['sportive_activity'];
 
 function calculCalorie($weight, $size, $age, $sex, $sportiveActivity)
 {
@@ -23,12 +29,8 @@ function calculCalorie($weight, $size, $age, $sex, $sportiveActivity)
     return $result;
 }
 
-
-function createMeal()
+function BMIcalc($weight, $size)
 {
-    if (!empty($_POST)) {
-        $calories = $_POST['calories'];
-        $meal_details = $_POST['details'];
-        $result = getInformationsMeal($calories, $meal_details);
-    }
+    $bmi = $weight / (($size * 100) * ($size * 100));
+    return $bmi;
 }
